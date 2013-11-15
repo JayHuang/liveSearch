5952c6641faeb05f1d1010ad5bef17c214c211fb
Jay Huang <jayhuang@example.com>
2013-11-14 16:22:44 -0800

    Added settings, minor styling changes

--- js/jquery.liveSearch.js
+++ js/jquery.liveSearch.js
@@ -1,28 +1,49 @@
 (function($) {
+	var settings = {
+		searchinput: '#search',
+		hide: true,
+		highlight: false,
+		highlightcolor: '#99ff99'
+	}
+
 	$.fn.liveSearch = function(options) {
 		'use strict';
 
-		var settings = $.extend({}, $.fn.liveSearch.defaults, options);
+		$.extend(settings, options);
 		var $searchTarget = $(this);
-		var $searchInput = $(settings.searchInput);
+		var $searchInput = $(settings.searchinput);
 		
 		search($searchTarget, $searchInput);
 		return this; // Return jQuery object for chaining
 	};
 
 	function search(searchTarget, searchInput) {
-		var items = searchTarget.children();
+		var $items = searchTarget.children();
 
-		searchInput.keyup(function(){
+		searchInput.keyup(function() {
 			var val = $.trim(searchInput.val()).replace(/ +/g, ' ').toLowerCase();
-			items.show().filter(function() {
+			var $remaining = $items.show().filter(function() {
 				var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
 				return !~text.indexOf(val);
-			}).hide();
+			})
+
+			var $match = $items.not($remaining);
+			
+			if(settings.hide)
+				$remaining.hide();
+			if(settings.highlight) {
+				if(val == '') {
+					reset($match);
+				} else {
+					$match.css('background-color', settings.highlightcolor);
+				}
+				$remaining.css('background-color', 'none');
+			}
 		});
 	}
 
-	$.fn.liveSearch.defaults = {
-		searchInput: '#search'
-	}
+	function reset(object) { //Reset all dynamic styling on jQuery object
+  		$(object).removeAttr('style');
+  	}
+
 })(jQuery);
